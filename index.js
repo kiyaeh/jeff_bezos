6e76ddcce95d4db888ab58954e43365308e1d3c7
kiyaeh <kiyaehcs@gmail.com>
2024-04-04 12:51:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "men",
+  });
+});
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(201).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+app.listen(8888, (err) => {
+  if (err) throw err;
+  console.log("Amazon se rserver running on port 5000,https://localhost:8888");
+});
